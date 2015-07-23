77903e1ea608903b1f88e2e58dea1c2536019e9d
Joe Masilotti <joseph.masilotti@gmail.com>
2015-07-23 06:29:19 -0400

    Convert AppDelegate to Swift
    which removes the need for main.m

---
+++ Test App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  JAMTestHelper
+//
+//  Created by Joe Masilotti on 7/23/15.
+//  Copyright © 2015 Masilotti.com. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window : UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        return true
+    }
+}
